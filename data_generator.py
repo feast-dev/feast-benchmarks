bbf5efab254a6d6429dae6c3d02b08b84213c2af
Tsotne Tabidze <tsotne@tecton.ai>
2021-12-07 17:27:22 -0800

    Add data generator & basic feast repo

---
+++ data_generator.py
@@ -0,0 +1,18 @@
+import pyarrow as pa
+import pyarrow.parquet as pq
+import pandas as pd
+import numpy as np
+from pathlib import Path
+
+def generate_data(num_rows: int, num_features: int, key_space: int) -> pd.DataFrame:
+    features = [f"feature_{i}" for i in range(num_features)]
+    columns = ["entity", "event_timestamp"] + features
+    df = pd.DataFrame(0, index=np.arange(num_rows), columns=columns)
+    df["event_timestamp"] = pd.to_datetime('today')
+    for column in ["entity"] + features:
+        df[column] = np.random.randint(1, key_space, num_rows)
+    return df
+
+if __name__ == "__main__":
+    df = generate_data(10**5, 250, 10**9)
+    df.to_parquet(Path(__file__).parent / "feature_repo/data/generated_data.parquet")
