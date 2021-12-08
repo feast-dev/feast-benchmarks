3e32faa8b48ac2d5cf964791ec605ec126b72416
pyalex <moskalenko.alexey@gmail.com>
2021-12-08 19:19:35 +0100

    update concurrency

--- java/run-benchmark.sh
+++ java/run-benchmark.sh
@@ -7,6 +7,7 @@ TOTAL_REQUESTS_NUM=${TOTAL_REQUESTS_NUM:-10000}
 UNIQUE_REQUESTS_NUM=${UNIQUE_REQUESTS_NUM:-1000}
 TARGET=${TARGET:-localhost:6566}
 
+trap "exit" INT
 
 single_run() {
 	echo "Entity rows: $1; Features: $2; Concurrency: $3; RPS: $4"
@@ -16,6 +17,13 @@ single_run() {
 		--features $2 \
 		--requests ${UNIQUE_REQUESTS_NUM} \
 		--output requests-$1-$2.json
+
+	echo "ghz ${GHZ_FLAGS}
+-n ${TOTAL_REQUESTS_NUM}
+--data-file requests-$1-$2.json
+--rps $4 \\
+-c $3
+$TARGET"
 
 	ghz ${GHZ_FLAGS}\
 		-n ${TOTAL_REQUESTS_NUM} \
@@ -29,16 +37,15 @@ single_run() {
 # single_run <entities> <features> <concurrency> <rps>
 
 
-CONCURRENCY=${CONCURRENCY:-10}
-echo "Freezing concurrency to $CONCURRENCY"
+echo "With freezed concurrency"
 
-for i in $(seq 100 100 1000); do single_run 1 50 $CONCURRENCY $i; done
+for i in $(seq 100 100 1000); do single_run 1 50 20 $i; done
 
-for i in $(seq 100 100 1000); do single_run 1 250 $CONCURRENCY $i; done
+for i in $(seq 100 100 1000); do single_run 1 250 20 $i; done
 
-for i in $(seq 100 100 500); do single_run 100 50 $CONCURRENCY $i; done
+for i in $(seq 100 100 500); do single_run 100 50 5 $i; done
 
-for i in $(seq 100 100 500); do single_run 100 250 $CONCURRENCY $i; done
+for i in $(seq 100 100 500); do single_run 100 250 5 $i; done
 
 
 echo "Maximum possible RPS"
@@ -47,6 +54,6 @@ for i in $(seq 10 10 50); do single_run 1 50 $i 0; done
 
 for i in $(seq 10 10 50); do single_run 1 250 $i 0; done
 
-for i in $(seq 10 10 50); do single_run 100 50 $i 0; done
+for i in $(seq 2 2 10); do single_run 100 50 $i 0; done
 
-for i in $(seq 10 10 50); do single_run 100 250 $i 0; done
+for i in $(seq 2 2 10); do single_run 100 250 $i 0; done
