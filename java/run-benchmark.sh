1369319ffacdef4ba8c9dabffeb99fed877993bc
pyalex <moskalenko.alexey@gmail.com>
2021-12-09 14:47:35 +0100

    update benchmark scenario

--- java/run-benchmark.sh
+++ java/run-benchmark.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 
-GHZ_FLAGS=${GHZ_FLAGS:-"--insecure -i protos/ --proto ./protos/ServingService.proto --call feast.serving.ServingService.GetOnlineFeaturesV2 --cpus=4 --skipFirst=100"}
+GHZ_FLAGS=${GHZ_FLAGS:-"--insecure -i protos/ --proto ./protos/ServingService.proto --call feast.serving.ServingService.GetOnlineFeaturesV2 --cpus=4 --skipFirst=100 --timeout=100ms"}
 
 TOTAL_REQUESTS_NUM=${TOTAL_REQUESTS_NUM:-10000}
 UNIQUE_REQUESTS_NUM=${UNIQUE_REQUESTS_NUM:-1000}
 TARGET=${TARGET:-localhost:6566}
+CONCURRENCY=${CONCURRENCY:-5}
 
 trap "exit" INT
 
@@ -37,18 +38,25 @@ $TARGET"
 # single_run <entities> <features> <concurrency> <rps>
 
 
-echo "With freezed concurrency"
+echo "Change only number of rows"
 
-for i in $(seq 100 100 1000); do single_run 1 50 20 $i; done
+single_run 1 50 $CONCURRENCY 100
 
-for i in $(seq 100 100 1000); do single_run 1 250 20 $i; done
-
-for i in $(seq 100 100 500); do single_run 100 50 5 $i; done
-
-for i in $(seq 100 100 500); do single_run 100 250 5 $i; done
+for i in $(seq 10 10 100); do single_run $i 50 $CONCURRENCY 100; done
 
 
-echo "Maximum possible RPS"
+echo "Change only number of features"
+
+for i in $(seq 50 50 250); do single_run 1 $i $CONCURRENCY 100; done
+
+
+echo "Change only number of requests"
+
+for i in $(seq 100 100 1000); do single_run 1 50 $CONCURRENCY $i; done
+
+
+
+echo "Fix uptime to 99.9% with 100ms timeout"
 
 for i in $(seq 10 10 50); do single_run 1 50 $i 0; done
 
