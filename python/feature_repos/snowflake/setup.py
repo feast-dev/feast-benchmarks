ade9af5106b95b1a15a813b9c56980369ebb7658
Miles Adkins <miles.adkins@snowflake.com>
2022-02-27 13:30:20 -0600

    fixed snowflake bug

--- python/feature_repos/snowflake/setup.py
+++ python/feature_repos/snowflake/setup.py
@@ -1,11 +1,12 @@
 from datetime import datetime
-from example import feature_views, feature_services
+from example import entity, feature_views, feature_services
 from feast import FeatureStore
 
 def main():
 
     fs = FeatureStore(repo_path=".")
 
+    fs.apply([entity])
     [fs.apply([fv]) for fv in feature_views]
     [fs.apply([fv]) for fv in feature_services]
 
